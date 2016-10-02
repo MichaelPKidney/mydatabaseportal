c311547e55e475d99e979230e2661cdad34d1ea1
MichaelPKidney <michaelpkidney@hotmail.com>
2016-10-01 20:18:10 -0400

    REST API

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/MyPortal',
+    'facebook': {
+        clientID: '1593541930946332',
+        clientSecret: '5439a51f6273c1a2cf09a8c75aba7f05',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
+
