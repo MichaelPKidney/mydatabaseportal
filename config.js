6c52113da7758c04f2a27a43285991a988e23b19
MichaelPKidney <michaelpkidney@hotmail.com>
2016-10-02 19:49:55 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/MyPortal',
+   'mongoUrl' : 'mongodb://admin:test@ds139655.mlab.com:39655/heroku_bz4sj28q',
     'facebook': {
         clientID: '1593541930946332',
         clientSecret: '5439a51f6273c1a2cf09a8c75aba7f05',
@@ -8,3 +8,7 @@ module.exports = {
     }
 }
 
+//'mongoUrl' : 'mongodb://localhost:27017/MyPortal',
+//
+
+// 'mongoUrl' : 'mongodb://admin:test@ds139655.mlab.com:39655/heroku_bz4sj28q',
